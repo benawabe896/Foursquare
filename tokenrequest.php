02bd4f4c3cbc37c1b0ad804cedb764a97af7121d
root <root@ip-10-245-25-34.ec2.internal>
2012-09-11 03:57:13 +0000

    api files

---
+++ tokenrequest.php
@@ -0,0 +1,45 @@
+<?php 
+	require_once("./src/FoursquareAPI.class.php");
+	
+	// This file is intended to be used as your redirect_uri for the client on Foursquare
+	
+	// Set your client key and secret
+	$client_key = "O2HBL2HGNDLYMLGM2BEKYTS45IRYHFIB3AGXUEJVHQETAJHX";
+	$client_secret = "MT0BJAQUJB0NSGQROIQZQQT5OQUBXQPPVLGOHSACFCBS0DPM";
+	$redirect_uri = "http://ec2-23-20-187-113.compute-1.amazonaws.com/Foursquare/tokenrequest.php";
+	
+	// Load the Foursquare API library
+	$foursquare = new FoursquareAPI($client_key,$client_secret);
+	
+	// If the link has been clicked, and we have a supplied code, use it to request a token
+	if(array_key_exists("code",$_GET)){
+		$token = $foursquare->GetToken($_GET['code'],$redirect_uri);
+	}
+	
+?>
+<!doctype html>
+<html>
+<head>
+	<title>PHP-Foursquare :: Token Request Example</title>
+</head>
+<body>
+<h1>Token Request Example</h1>
+<p>
+	<?php 
+	// If we have not received a token, display the link for Foursquare webauth
+	if(!isset($token)){ 
+		echo "<a href='".$foursquare->AuthenticationLink($redirect_uri)."'>Connect to this app via Foursquare</a>";
+	// Otherwise display the token
+	}else{
+		echo "Your auth token: $token";
+	}
+	
+	?>
+	
+</p>
+<hr />
+<?php 
+	
+?>
+</body>
+</html>
